694c28e54dd7a7ac191545d298a1669d07ca5c35
Steven Masley <stevenmasley@gmail.com>
2023-12-12 11:10:58 -0600

    Unstable let's go!

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    home-manager =  {
-      url = "github:nix-community/home-manager/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -16,14 +16,16 @@
         inherit system;
       };
       lib = nixpkgs.lib;
-    in {
+    in
+    {
       nixosConfigurations = {
         desktop-amd64 = lib.nixosSystem {
           inherit system;
-          modules = [ 
+          modules = [
             ./hardware/hardware-configuration.nix
-            ./nixos/configuration.nix 
-            home-manager.nixosModules.home-manager {
+            ./nixos/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.steven = import ./home/steven.nix; # { inherit pkgs; };
