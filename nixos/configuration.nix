623645bbf72fd75463564a7216a2fcaf1455ccb5
Steven Masley <stevenmasley@gmail.com>
2024-07-04 12:09:44 -0500

    fixup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -359,7 +359,7 @@ in
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
-  hardware.opengl.enable32Bit = true; # Enables support for 32bit libs that steam uses
+  hardware.graphics.enable32Bit = true; # Enables support for 32bit libs that steam uses
 
   nix = {
     package = pkgs.nixFlakes;
