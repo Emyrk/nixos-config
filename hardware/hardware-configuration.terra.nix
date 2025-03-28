56cd25311afaa18c795517d7fc48b0e2808fa09b
Steven Masley <stevenmasley@gmail.com>
2025-03-28 16:02:34 -0500

    switch back to wayland

--- hardware/hardware-configuration.terra.nix
+++ hardware/hardware-configuration.terra.nix
@@ -29,8 +29,10 @@
       fsType = "vfat";
     };
 
+
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/df262190-add8-4c58-a7b2-b55e055a0ce2"; }];
+    [];
+  # was [{ device = "/dev/disk/by-uuid/df262190-add8-4c58-a7b2-b55e055a0ce2"; }]
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
