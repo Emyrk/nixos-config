82358cf1c531ce905634b424addcf2a08c88b01d
Steven Masley <stevenmasley@gmail.com>
2025-04-01 16:47:42 -0500

    add gpu fan control

--- nixos/amd.pkgs.nix
+++ nixos/amd.pkgs.nix
@@ -6,5 +6,10 @@
     radeontop
     nvtopPackages.amd
     lm_sensors
+    lact
   ];
+  # AMD Fan speed control
+  # https://wiki.nixos.org/wiki/AMD_GPU
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = [ "multi-user.target" ];
 }
